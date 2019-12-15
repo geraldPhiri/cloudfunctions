c8bafd02834a3c59e2b40087f56e920db12b19c6
Gerald Phiri <48890237+geraldPhiri@users.noreply.github.com>
2019-12-15 10:49:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.liveUrlChange = functions.database.ref('ProductionDB/Stock/').onWrite((event) => {
+    // Exit if data is deleted.
+    //if (!change.after.exists()) return null;
+
+    // Grab the current value of what was written to the Realtime Database.
+    //const value = change.after.val();
+    //console.log('The liveurl value is now', value);
+
+    // Build the messaging notification, sending to the 'all' topic.
+    var message = {
+        notification: {
+            title: 'Database update',
+            body: 'Please update prices on device'
+        },
+    };
+
+    // Send the message.
+    return admin.messaging().sendToTopic('update',message)
+        .then((message) => console.log('Successfully sent message:', message)
+        )
+        .catch((error) => {
+            console.error('Error sending message:', error);
+        });
+});
